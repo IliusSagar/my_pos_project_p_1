1ec4b97b7b567618f5758bb6bc46c9e5d60a1717
IliusSagar <sagar01830@gmail.com>
2021-09-12 22:49:25 -0700

    expense module developed

--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,26 @@ Route::get('/delete-product/{id}','ProductController@DeleteProduct');
 Route::get('/view-product/{id}','ProductController@ViewProduct');
 Route::get('/edit-product/{id}', 'ProductController@EditProduct');
 Route::post('/update-product/{id}','ProductController@UpdateProduct');
+
+
+//Expense routes are here------------------------
+Route::get('/add-expense','ExpenseController@AddExpense')->name('add.expense');
+Route::post('/insert-expense','ExpenseController@InsertExpense');
+Route::get('/today-expense','ExpenseController@TodayExpense')->name('today.expense');
+Route::get('/edit-today-expense/{id}', 'ExpenseController@EditTodayExpense');
+Route::post('/update-expense/{id}','ExpenseController@UpdateExpense');
+Route::get('/monthly-expense','ExpenseController@MonthlyExpense')->name('monthly.expense');
+Route::get('/yearly-expense','ExpenseController@YearlyExpense')->name('yearly.expense');
+//monthly more expenses----------------
+Route::get('/january-expense','ExpenseController@JanuaryExpense')->name('january.expense');
+Route::get('/february-expense','ExpenseController@FebruaryExpense')->name('february.expense');
+Route::get('/march-expense','ExpenseController@MarchExpense')->name('march.expense');
+Route::get('/april-expense','ExpenseController@AprilExpense')->name('april.expense');
+Route::get('/may-expense','ExpenseController@MayExpense')->name('may.expense');
+Route::get('/june-expense','ExpenseController@JuneExpense')->name('june.expense');
+Route::get('/july-expense','ExpenseController@JulyExpense')->name('july.expense');
+Route::get('/august-expense','ExpenseController@AugustExpense')->name('august.expense');
+Route::get('/september-expense','ExpenseController@SeptemberExpense')->name('september.expense');
+Route::get('/october-expense','ExpenseController@OctoberExpense')->name('october.expense');
+Route::get('/november-expense','ExpenseController@NovemberExpense')->name('november.expense');
+Route::get('/december-expense','ExpenseController@DecemberExpense')->name('december.expense');
